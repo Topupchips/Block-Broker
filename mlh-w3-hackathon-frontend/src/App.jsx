9b50a74564c590acb71abee338ba9c9c95ae60ad
hlalljie <hlalljie@gmail.com>
2023-12-29 15:45:00 -0800

    Added nav

--- mlh-w3-hackathon-frontend/src/App.jsx
+++ mlh-w3-hackathon-frontend/src/App.jsx
@@ -5,17 +5,27 @@ function App() {
 
   return (
     <>
-      <Nav/>
+      <HeaderNav/>
     </>
   )
 }
 
-function Nav(){
+function HeaderNav(){
   return (
     <>
-      
+      <header className="App-header">
+        <h3 className="header-nav-logo">Your Contract</h3>
+        <div className="header-nav-items">
+          <a>Home</a>
+          <a>About</a>
+          <a>Contracts</a>
+          <a>Faqs</a>
+          <a>Login</a>
+          <a>Connect</a>
+        </div>
+      </header>
     </>
-  )
+  );
 }
 
 export default App
