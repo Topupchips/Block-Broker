72b3685b03d0ee8921ebd823aa6337c4a2691de5
hlalljie <hlalljie@gmail.com>
2023-12-30 12:16:04 -0800

    Available contracts dynamically load

--- mlh-w3-hackathon-frontend/src/pages/Contracts.jsx
+++ mlh-w3-hackathon-frontend/src/pages/Contracts.jsx
@@ -12,10 +12,55 @@ const availableContracts = [
         title: "Example Contract 2", 
         price: "$3,000",
         description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+    },
+    {
+        title: "Example Contract 3", 
+        price: "$3,000",
+        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+    },
+    {
+        title: "Example Contract 4", 
+        price: "$3,000",
+        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+    },
+    {
+        title: "Example Contract 5", 
+        price: "$3,000",
+        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+    },
+    {
+        title: "Example Contract 6", 
+        price: "$3,000",
+        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+    },
+    {
+        title: "Example Contract 7", 
+        price: "$3,000",
+        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+    },
+    {
+        title: "Example Contract 8", 
+        price: "$3,000",
+        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+    },
+    {
+        title: "Example Contract 9", 
+        price: "$3,000",
+        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+    },
+    {
+        title: "Example Contract 10", 
+        price: "$3,000",
+        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+    },
+    {
+        title: "Example Contract 11", 
+        price: "$3,000",
+        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
     }
 ];
 function Contracts() {
-
+    
     return (
       <>
         <Header/>
@@ -34,12 +79,20 @@ function Contracts() {
 
 function AvailableContractsBlock(props) {
 
+    let numToRender = Math.min(props.loadAmount, availableContracts.length)
+    const componentsToRender = [];
+    for (let i = 0; i < numToRender; i++) {
+        // note: we are adding a key prop here to allow react to uniquely identify each
+        // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+        componentsToRender .push(<AvailableContract contractInfo={availableContracts[i]}/>);
+    }
     return (
       <>
         <div className="AvailableContractsBlockTop">
             <h2 className="contractsHeader">Available Contracts</h2>
         </div>
-        <AvailableContract contractInfo={availableContracts[]}/>
+        {componentsToRender}
+        
       </>
     );
 }
