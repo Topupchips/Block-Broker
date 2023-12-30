fe9c3aa346efafd9c7b7a5ed4be92a81fd8ac6ca
hlalljie <hlalljie@gmail.com>
2023-12-30 14:59:04 -0800

    exmpl contracts refelect real and started list org

--- mlh-w3-hackathon-frontend/src/pages/Contracts.jsx
+++ mlh-w3-hackathon-frontend/src/pages/Contracts.jsx
@@ -2,65 +2,155 @@ import "./Contracts.css"
 
 //Section Imports
 import Header from '../sections/Header.jsx';
-const availableContracts = [
+const userId = "34345675";
+const allContracts = [
     {
-        title: "Example Contract", 
-        price: "$1,000",
-        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        jobId: 1,
+        client: "12345675",
+        freelancer: null,
+        applied: [],
+        title: "Example Contract 1", 
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "open",
+        verifiedByClient: false
     },
     {
+        jobId: 2,
+        client: "12345675",
+        freelancer: null,
+        applied: [],
         title: "Example Contract 2", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "open",
+        verifiedByClient: false
     },
     {
+        jobId: 3,
+        client: "34345675",
+        freelancer: null,
+        applied: [],
         title: "Example Contract 3", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "open",
+        verifiedByClient: false
     },
     {
+        jobId: 4,
+        client: "34345675",
+        freelancer: null,
+        applied: [],
         title: "Example Contract 4", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "open",
+        verifiedByClient: false
     },
     {
+        jobId: 5,
+        client: "52856954",
+        freelancer: "34345675",
+        applied: [],
         title: "Example Contract 5", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "in progress",
+        verifiedByClient: false
     },
     {
+        jobId: 6,
+        client: "52856954",
+        freelancer: "343683863",
+        applied: [],
         title: "Example Contract 6", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "completed",
+        verifiedByClient: false
     },
     {
+        jobId: 7,
+        client: "63556361",
+        freelancer: "343683863",
+        applied: [],
         title: "Example Contract 7", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "approved",
+        verifiedByClient: false
     },
     {
+        jobId: 8,
+        client: "52856954",
+        freelancer: null,
+        applied: [],
         title: "Example Contract 8", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "open",
+        verifiedByClient: false
     },
     {
+        jobId: 9,
+        client: "62345455",
+        freelancer: null,
+        applied: [],
         title: "Example Contract 9", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "open",
+        verifiedByClient: false
     },
     {
+        jobId: 10,
+        client: "62345455",
+        freelancer: null,
+        applied: [],
         title: "Example Contract 10", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "open",
+        verifiedByClient: false
     },
     {
+        jobId: 11,
+        client: "62345455",
+        freelancer: null,
+        applied: [],
         title: "Example Contract 11", 
-        price: "$3,000",
-        description: "Lorem rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum."
+        budget: "$1,000",
+        description: "Vivamus rutrum mollis suscipit. Nulla ut nisl tellus. Ut sagittis purus risus, malesuada rutrum mi gravida eu. Morbi imperdiet mi justo, at tincidunt neque hendrerit vel. Quisque nec nibh ut nunc mattis mattis. In laoreet sapien ac laoreet bibendum.",
+        proof: "something",
+        status: "open",
+        verifiedByClient: false
     }
 ];
+
+const availableContracts = [];
+
+function organizeContracts(contractsList){
+    availableContracts.length = 0; // clears array
+    for (let contract of contractsList){
+        if (contract.status == "open"){
+            availableContracts.push(contract);
+        }
+    }
+}
+
 function Contracts() {
-    
+    organizeContracts(allContracts);
     return (
       <>
         <Header/>
@@ -68,7 +158,10 @@ function Contracts() {
             <div className="contractsTop">
                 <h1 className="contractsHeader">Contracts</h1>
             </div>
-            <AvailableContractsBlock loadAmount={10}/>
+            <div className="AvailableContractsBlockTop">
+                <h2 className="contractsHeader">Available Contracts</h2>
+            </div>
+            <AvailableContractsBlock loadAmount={20}/>
         </div>
         
       </>
@@ -88,9 +181,6 @@ function AvailableContractsBlock(props) {
     }
     return (
       <>
-        <div className="AvailableContractsBlockTop">
-            <h2 className="contractsHeader">Available Contracts</h2>
-        </div>
         {componentsToRender}
         
       </>
