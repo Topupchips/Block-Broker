a6b0654857e66a101e4b9fc202e0ca595e718692
Ansh <138444853+Topupchips@users.noreply.github.com>
2023-12-30 17:39:15 -0400

    added Rainbow Kit

--- mlh-w3-hackathon-frontend/src/main.jsx
+++ mlh-w3-hackathon-frontend/src/main.jsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
@@ -8,6 +9,23 @@ import {
 } from "react-router-dom";
 import Contracts from './pages/Contracts.jsx';
 import LoginForm from './pages/loginform.jsx';
+
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  base,
+  zora,
+} from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
 const router = createBrowserRouter([
   {
@@ -23,9 +41,29 @@ const router = createBrowserRouter([
     element: <LoginForm/>,
   }
 ]);
-
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, base, zora],
+  [
+   
+    publicProvider()
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  projectId: 'YOUR_PROJECT_ID',
+  chains
+});
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <WagmiConfig config={wagmiConfig}>
+  <RainbowKitProvider chains={chains}>
+  <React.StrictMode>  
     <RouterProvider router={router}/>
   </React.StrictMode>,
+  </RainbowKitProvider>
+    </WagmiConfig>
 )
