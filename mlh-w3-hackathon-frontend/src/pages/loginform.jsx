c0c03093424afa781150e03000372bc82da06617
Yuko_Hozumi <84846239+Saumya-Singh17@users.noreply.github.com>
2023-12-30 16:59:30 +0530

    Add files via upload
    structure of the login form

---
+++ mlh-w3-hackathon-frontend/src/pages/loginform.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import './LoginForm.css'
+import { FaUser, FaLock } from "react-icons/fa";
+
+
+const LoginForm = () => {
+  return(
+    <div className="Wrapper">
+        <form action="">
+            <h1> Login</h1>
+            <div className="Input-box">
+                <input type="text" placeholder="Username" required/>
+                <FaUser className="icon" />
+            </div>
+            <div className="Input-box">
+                <input type="password" placeholder="Password" required />
+                <FaLock className="icon" />
+            </div>
+            <div className="Remember-forgot">
+                <label> <input type="checkbox" />
+                Remeber me
+                </label> 
+                <a href="#">Forgot Password?</a>  
+             </div>
+
+             <button type="submit">Login</button>
+
+             <div className="register-link">
+                <p>Don't have an account?
+                    <a href="#"> Register here</a>
+                </p>
+             </div>
+            </form> 
+    
+        
+         </div>
+  );
+};
+
+export default LoginForm;  
