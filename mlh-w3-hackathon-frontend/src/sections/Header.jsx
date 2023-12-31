a650d1ee754b24f763582dc2c2902cf7ab58e89d
hlalljie <hlalljie@gmail.com>
2023-12-31 03:04:33 -0800

    Changed logomark

--- mlh-w3-hackathon-frontend/src/sections/Header.jsx
+++ mlh-w3-hackathon-frontend/src/sections/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
     return (
       <>
         <header className="App-header">
-          <h3 className="header-nav-logo">Your Contract</h3>
+          <h3 className="header-nav-logo">Block Broker</h3>
           <div className="header-nav-items">
             <Link to="/">Home</Link>
             <Link to="/home#about">About</Link>
