b59d8b8a27324cff7411e8b615265e3c75671932
hlalljie <hlalljie@gmail.com>
2023-12-31 04:35:49 -0800

    Added logo link

--- mlh-w3-hackathon-frontend/src/sections/Header.jsx
+++ mlh-w3-hackathon-frontend/src/sections/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
     return (
       <>
         <header className="App-header">
-          <h3 className="header-nav-logo">Block Broker</h3>
+          <Link to="/home#logo"><h3 className="header-nav-logo" id="logo">Block Broker</h3></Link>
           <div className="header-nav-items">
             <Link to="/">Home</Link>
             <Link to="/home#about">About</Link>
